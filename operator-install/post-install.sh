b985ea52b6b994a8b38c8b15b15a5836bd85f1d4
Balazs Szeti <szeti.balazs@gmail.com>
2023-05-17 17:47:36 -0400

    fix

--- operator-install/post-install.sh
+++ operator-install/post-install.sh
@@ -1,10 +1,10 @@
 if [ "oc get subscription.operators.coreos.com amq-broker-rhel8 -o jsonpath='{.spec.installPlanApproval}')" == "Manual" ]; then
   echo 'Waiting for InstallPlan to show up.'
-  WHILECMD='[ -z "oc get installplan -l operators.coreos.com/amq-broker-rhel8.myproject -oname)" ]'
+  WHILECMD='[ -z "$(oc get installplan -l operators.coreos.com/amq-broker-rhel8.myproject -oname)" ]'
   timeout 15m sh -c "while $WHILECMD; do echo Waiting; sleep 10; done"
 
   # There should be only one InstallPlan if pre-install.sh was run at the beginnig.
-  installplans=oc get installplan -l operators.coreos.com/amq-broker-rhel8.myproject -oname)
+  installplans=$(oc get installplan -l operators.coreos.com/amq-broker-rhel8.myproject -oname)
   if [ -z "$installplans"]; then
     echo "No InstallPlan was found for operator with label operators.coreos.com/amq-broker-rhel8.myproject. This indicates a failure about operator installation."
     exit 1
