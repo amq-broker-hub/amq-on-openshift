d8ae1021bdca62b88571bc17d5458a3730da3a71
Balazs Szeti <szeti.balazs@gmail.com>
2023-05-25 17:14:12 -0400

    delete installplans

--- operator-install/pre-install-multisub.sh
+++ operator-install/pre-install-multisub.sh
@@ -1,4 +1,6 @@
-# Delete all other Subscriptions to avoid unwanted upgrades. This will also remove all InstallPlans
+# Delete all other Subscriptions to avoid unwanted upgrades. 
 oc delete subscription.operators.coreos.com --all
+# Delete all InstallPlans
+oc delete installplan.operators.coreos.com --all
 # Delete the ClusterServiceVersion that we're reinstalling to avoid "clusterserviceversion exists and is not referenced by a subscription" error. This also deletes the running operator.
 oc delete csv -l operator-activemqartemis
