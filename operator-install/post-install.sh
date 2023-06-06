ca135d2d3aa789bc140378a8fb4b5a5cd1eefc86
Balazs Szeti <szeti.balazs@gmail.com>
2023-06-05 23:01:19 -0400

    Helm chart amq-operator-helm-chart

--- operator-install/post-install.sh
+++ operator-install/post-install.sh
@@ -1,38 +1,36 @@
 # This scripts checks if "startingCSV" is set on the Subscription and in that case approves only the InstallPlan with the wanted CSV
 
-if [ "oc get subscription.operators.coreos.com amq-broker-rhel8 -o jsonpath='{.spec.installPlanApproval}')" == "Manual" ]; then
-  echo 'Waiting for InstallPlan to show up.'
-  WHILECMD='[ -z "$(oc get installplan -l operators.coreos.com/amq-broker-rhel8.myproject -oname)" ]'
-  timeout 15m sh -c "while $WHILECMD; do echo Waiting; sleep 10; done"
+if [[ "$(oc get subscription.operators.coreos.com amq-broker-rhel8 -o jsonpath='{.spec.installPlanApproval}')" == "Manual" ]]; then
+  echo 'Wait for InstallPlan'
+  WHILECMD='[ -z "$(oc get subscriptions.operators.coreos.com amq-broker-rhel8 -o jsonpath={.status.installPlanRef.name})" ]'
+  timeout 5m sh -c "while $WHILECMD; do echo Waiting; sleep 10; done"
 
-  # Search InstallPlan based on startingCSV
-  startingCSV=$(oc get subscription.operators.coreos.com amq-broker-rhel8 -o jsonpath='{.spec.startingCSV}')
-  if [ -z "$startingCSV" ]; then
-    # no startingCSV: check all InstallPlans
-    installplans=$(oc get installplan -l operators.coreos.com/amq-broker-rhel8.myproject -oname)
-  else
-    # startingCSV is set: get the InstallPlan with matching "clusterServiceVersionNames" to avoid unexpected upgrades.
-    # This should return max one InstallPlan, unless multiple operators are installed in the same namespace
-    installplans=$(oc get ip -ojson | jq -r '.items[] | select( .spec.clusterServiceVersionNames[] | contains("amq-broker-operator.v7.11.0-opr-2"))  | .metadata.name')
-    # Could also filter for "approved == false" in one command, then we wouldn't need to check later
-    #installplans=$(oc get ip -ojson | jq -r '.items[] | select( (.spec.clusterServiceVersionNames[] | contains("amq-broker-operator.v7.11.0-opr-3")) and .spec.approved == false )  | .metadata.name')
-  fi
-  
+  # Get last InstallPlan for Subscription
+  installplan=$(oc get subscriptions.operators.coreos.com amq-broker-rhel8 -o jsonpath={.status.installPlanRef.name})
   # No InstallPlan found
-  if [ -z "$installplans"]; then
-    echo "No InstallPlan was found for operator with label operators.coreos.com/amq-broker-rhel8.myproject. This indicates a failure about operator installation."
+  if [ -z "$installplan" ]; then
+    echo "No InstallPlan was found for subscription amq-broker-rhel8. This indicates a failure about operator installation."
     exit 1
   fi
-  
-  # Approve the InstallPlans
-  for installplan in $installplans
-  do
-    if [ "$(oc get $installplan -o jsonpath='{.spec.approved}')" == "false" ]; then
-      echo "Approving install plan $installplan"
-      oc patch $installplan --type=json -p='[{"op":"replace","path": "/spec/approved", "value": true}]'
-    else
-      echo "Install Plan '$installplan' was already approved"
-    fi
-  done
 
+  # If startingCSV is set, patch InstallPlan only with matching "clusterServiceVersionNames" to avoid unexpected upgrades.
+  startingCSV=$(oc get subscription.operators.coreos.com amq-broker-rhel8 -o jsonpath='{.spec.startingCSV}')
+  if [ -n "$startingCSV" ]; then
+    echo "Check if InstallPlan $installplan has CSV $startingCSV"
+    installplan=$(oc get installplan.operators.coreos.com $installplan -ojson | jq -r 'select( .spec.clusterServiceVersionNames[] | contains("'$startingCSV'")) | .metadata.name')
+    # No InstallPlan found
+    if [ -z "$installplan" ]; then
+      echo "InstallPlan doesn't have expected CSV $startingCSV. Won't approve."
+      exit 0
+    fi
+  fi
+  
+  # Approve the InstallPlan
+  if [[ "$(oc get installplan.operators.coreos.com $installplan -o jsonpath='{.spec.approved}')" == "false" ]]; then
+    echo "Approving InstallPlan $installplan"
+    oc patch installplan.operators.coreos.com $installplan --type=json -p='[{"op":"replace","path": "/spec/approved", "value": true}]'
+  else
+    echo "InstallPlan $installplan was already approved"
+  fi
+  
 fi
